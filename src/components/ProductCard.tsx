68a65a86f7ad9836aac95bec5d271ab8c116796a
Gabriel Thomé <gabrielallan.thome@gmail.com>
2022-09-25 06:23:29 -0300

    feat: ADD TO CART button actually adds to the cart

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import { Typography, Card, CardMedia, Button } from "@mui/material";
-import React from "react";
-import { ReactElement } from "react";
+import React, { ReactElement } from "react";
 import { numberToMoney } from "../utils"
-
+import { useShoppingCartContext, ShoppingCartActionType } from "../context/ShoppingCart"
+import { API } from "../API"
 interface ProductCardProps {
+  productId: number
     productName: string
     imgSrc: string
     productDescription: string
@@ -11,7 +12,25 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = (props: ProductCardProps): ReactElement => {
-    return (
+  const { dispatch } = useShoppingCartContext()
+
+  const handleAddToCartClick = () => {
+    dispatch({ type: ShoppingCartActionType.ADD, productId: props.productId, productName: props.productName })
+    
+    API.addToCart(props.productId)
+      .then(res => {
+        if (res.status !== 200) {
+          dispatch({ type: ShoppingCartActionType.REMOVE, productId: props.productId })
+          alert("Something went wrong while trying to add your product to the cart.")
+        }
+      })
+      .catch(() => {
+        dispatch({ type: ShoppingCartActionType.REMOVE, productId: props.productId })
+        alert("Something went wrong while trying to add your product to the cart. Check your internet connection and try again later")
+      })
+  }
+
+  return (
         <Card sx={{ maxWidth: 650, padding: "1em" }}>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>{props.productName}</Typography>
         <CardMedia
@@ -23,7 +42,7 @@ const ProductCard: React.FC<ProductCardProps> = (props: ProductCardProps): React
         />            
         <Typography sx={{ marginBottom: 1.5 }}>{props.productDescription}</Typography>
         <Typography>{numberToMoney(props.productPrice)}</Typography>
-        <Button variant="contained">ADD TO CART</Button>
+        <Button variant="contained" onClick={handleAddToCartClick}>ADD TO CART</Button>
     </Card>
   )
 }
